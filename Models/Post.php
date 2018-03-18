796138110f1c9248dbcdf4104cc2824db35233aa
thiagodemas <thiago.demas7@gmail.com>
2018-03-18 14:20:02 -0300

    model post

---
+++ Models/Post.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: thiagodemas
+ * Date: 18/03/18
+ * Time: 13:27
+ */
+require_once './DataBase/Conection.php';
+class Post
+{
+    protected $table;
+    public $title;
+    public $subtitle;
+    public $text;
+
+    /**
+     * @return mixed
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * @param mixed $title
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getSubtitle()
+    {
+        return $this->subtitle;
+    }
+
+    /**
+     * @param mixed $subtitle
+     */
+    public function setSubtitle($subtitle)
+    {
+        $this->subtitle = $subtitle;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getText()
+    {
+        return $this->text;
+    }
+
+    /**
+     * @param mixed $text
+     */
+    public function setText($text)
+    {
+        $this->text = $text;
+    }
+
+
+}
